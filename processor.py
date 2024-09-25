3eda76f94fe0700721ef819ce320175cb2a6f6ef
SwastikMajumder <75435931+SwastikMajumder@users.noreply.github.com>
2024-09-25 22:20:13 +0530

    Update processor.py

--- processor.py
+++ processor.py
@@ -2,6 +2,6 @@ import numpy as np
 import base64
 
 def pre_process(input):
-    return np.frombuffer(base64.b64decode(input), dtype=dtype)
+    return np.frombuffer(base64.b64decode(input), dtype=np.float32)
 def post_process(input):
     return base64.b64encode(input.tobytes()).decode('utf-8')
